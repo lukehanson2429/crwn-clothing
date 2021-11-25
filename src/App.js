b9b6438ceea886c85eba387967280f27728450b0
Luke Hanson <lukehanson2429@gmail.com>
2021-11-25 17:21:43 +0000

    installed latest react-render-dom & rendered routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return <div>
-    <HomePage />
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
   </div>
-};
+);
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes>
+    </div>
+
+  );
+}
 
 export default App;
