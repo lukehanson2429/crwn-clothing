a276d4e797254c4afc77f1d0d0f100bd2abfc1b5
Luke Hanson <lukehanson2429@gmail.com>
2021-11-29 15:53:07 +0000

    created sign in page with sign in google, styled google sign in button with sass

--- src/App.js
+++ src/App.js
@@ -22,20 +22,20 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+      this.setState({ currentUser: user });
 
       console.log(user);
-    })
+    });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  
+
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,9 +52,10 @@ class SignIn extends React.Component {
                         label='password'
                         required
                     />
-
+                    <div className="buttons">
                     <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Sign In with Google</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         );
