fa8d96723110453895e0208bc4a330c2846836bb
Luke Hanson <lukehanson2429@gmail.com>
2021-12-01 13:29:01 +0000

    impemented firebase utils, included ability to store auth users into firebase database

--- src/App.js
+++ src/App.js
@@ -32,10 +32,12 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+
+          console.log(this.state);
         });
       }
 
-      this.setState({currentUser: userAuth});
+      this.setState({ currentUser: userAuth });
     });
   }
 
